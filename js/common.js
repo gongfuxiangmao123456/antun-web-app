69c9b0fb385daa2bbaa2e0eb3bde26cb991ee06b
Administrator <m15311952785@163.com>
2017-11-29 15:55:38 +0800

    pc神州畅行 url2

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 // server = 'http://47.94.250.65/antun';
 //server = 'http://localhost:9092';
-server = 'http://192.168.141.201:8081';
+server = 'http://192.168.141.201:8081/antun';
 
 url_user=server+"/users";
 url_area=server+"/areas";
